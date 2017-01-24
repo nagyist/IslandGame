2cbe7168f5fd5bf88ccf10c8f235bebdb636be0b
RavengerINC <jamesmdalziel@gmail.com>
2017-01-23 23:09:07 -0400

    23-01-17
    Roads now angled between points

--- Assets/Production Assets/Scripts/GameBoard.cs
+++ Assets/Production Assets/Scripts/GameBoard.cs
@@ -215,6 +215,7 @@ public class GameBoard : MonoBehaviour {
             GameObject go = (GameObject)Instantiate(roadLocationGeo, new Vector3((edge.edgePoints[0].location.x + edge.edgePoints[1].location.x) / 2, 0, (edge.edgePoints[0].location.z + edge.edgePoints[1].location.z) / 2), new Quaternion());
             go.transform.parent = gameBoard.transform;
             go.transform.name = "Road: " + edge.edgePoints[0] +" to " + edge.edgePoints[1];
+            go.transform.Rotate(new Vector3(0, bearing(edge.edgePoints[0].location, edge.edgePoints[1].location), 0));
         }
     }
 
@@ -271,17 +272,24 @@ public class GameBoard : MonoBehaviour {
         }
     }
 
-    /*
-    double bearing(double a1, double a2, double b1, double b2) {
-    static const double TWOPI = 6.2831853071795865;
-    static const double RAD2DEG = 57.2957795130823209;
-    // if (a1 = b1 and a2 = b2) throw an error 
-    double theta = atan2(b1 - a1, a2 - b2);
-    if (theta < 0.0)
-        theta += TWOPI;
-    return RAD2DEG * theta;
+    //Find bearing between two points
+    float bearing(Vector3 point1, Vector3 point2) {
+
+        const float TWOPI = 6.2831853071795865f;
+        const float RAD2DEG = 57.2957795130823209f;
+
+        if (point1 == point2) {
+            return 0.0f;
+        }
+
+        float theta = Mathf.Atan2(point1.x - point2.x, point1.z - point2.z);
+
+        if (theta < 0.0) {
+            theta += TWOPI;
+        }
+
+        return RAD2DEG * theta;
     }
-    */
 
 }
 
